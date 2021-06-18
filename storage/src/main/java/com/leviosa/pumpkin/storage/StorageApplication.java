b16245786416be7e2b935b9bc0c9836e5bd85508
LeviosaPumpkin <nastya.khramtcova@gmail.com>
2021-06-18 21:06:36 +0300

    initial commit

---
+++ storage/src/main/java/com/leviosa/pumpkin/storage/StorageApplication.java
@@ -0,0 +1,13 @@
+package com.leviosa.pumpkin.storage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorageApplication.class, args);
+	}
+
+}
